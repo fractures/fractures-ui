c219edfee51a1dcdb4521234916f13caf48cfeef
Krisztian Puska <pyx@pyx.space>
2018-02-10 23:53:07 +0100

    Add todo

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 * Fast, working prototypes
 
 Quick mock on top of (fractures)[https://github.com/fractures/fractures].
+
+TODO
+
+* [ ] Finalize checkbox
+* [ ] Radio
+* [ ] Input
+* [ ] Dropdown
+* [ ] Date
+* [ ] Textarea
+* [ ] Password
+* [ ] Process
+* [ ] Range
+* [ ] Button group
+* [ ] Button rounded
+* [ ] Button consider cleaning up multiple types
+* [ ] Tooltip
+* [ ] Themes
+* [ ] Copy
+* [ ] Distribute
--- pages/index.js
+++ pages/index.js
@@ -50,19 +50,15 @@ class Index extends Component {
 						<h1 className="my-2">Buttons</h1>
 						<div className="flex flex-gap-1 my-1">
 							<Button type="primary" value="Primary" />
-							<Button type="secondary" value="Secondary" />
-						</div>
-						<div className="flex flex-gap-1 my-1">
-							<Button type="primary" value="Primary small" small={ true } />
-							<Button type="secondary" value="Secondary small" small={ true } />
-						</div>
-						<div className="flex flex-gap-1 my-1">
-							<Button type="primary" value="Disabled" disabled={ true } />
-							<Button type="secondary" value="Disabled" disabled={ true } />
-						</div>
-						<div className="flex flex-gap-1 my-1">
 							<Button type="primary" value="Active" isActive={ true } />
+							<Button type="primary" value="Disabled" disabled={ true } />
+							<Button type="primary" value="Primary small" small={ true } />
+						</div>
+						<div className="flex flex-gap-1 my-1">
+							<Button type="secondary" value="Secondary" />
 							<Button type="secondary" value="Active" isActive={ true } />
+							<Button type="secondary" value="Disabled" disabled={ true } />
+							<Button type="secondary" value="Secondary small" small={ true } />
 						</div>
 					</div>
 					<div className="my-4">
@@ -92,10 +88,14 @@ class Index extends Component {
 						--shade-6: ${ themes[this.state.theme][6] };
 					}
 
-					body,
+					/* body,
 					input {
 						font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif,
 							"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+					} */
+
+					input {
+						font-family: Roboto;
 					}
 				`}</style>
 			</div>
