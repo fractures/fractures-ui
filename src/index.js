3822a6dd871f4fa98ddfe17789d235657e45a2eb
Krisztian Puska <pyx@pyx.space>
2018-02-26 18:50:11 +0100

    Move under src for nicer webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Button from "./components/Button"
+import ButtonGroup from "./components/ButtonGroup"
+import Checkbox from "./components/Checkbox"
+import Container from "./components/Container"
+import Fractures from "./components/Fractures"
+import Input from "./components/Input"
+import Progress from "./components/Progress"
+import Radio from "./components/Radio"
+import Range from "./components/Range"
+
+export { Button, ButtonGroup, Checkbox, Container, Fractures, Input, Progress, Radio, Range }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-	entry: "./components/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "index.js",
