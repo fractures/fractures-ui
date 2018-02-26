eaee6c9c77d0f3e93a459951f80b90c066cb50f1
Krisztian Puska <pyx@pyx.space>
2018-02-26 22:24:26 +0100

    Button as stateless

--- src/components/Button.js
+++ src/components/Button.js
@@ -36,178 +36,176 @@ const IconLoading = props => (
 	</svg>
 )
 
-class Button extends Component {
-	render() {
-		const buttonClasses = cc({
-			"fr-btn": true,
-			"fr-btn-primary": this.props.type === "primary",
-			"fr-btn-secondary": this.props.type === "secondary",
-			"fr-btn--small": this.props.small,
-			"fr-btn--disabled": this.props.disabled,
-			"fr-btn--loading": this.props.isLoading,
-			"fr-btn--active": this.props.isActive,
-			"fr-btn--radius": !this.props.isRounded,
-			"radius-max": this.props.isRounded
-		})
+const Button = props => {
+	const buttonClasses = cc({
+		"fr-btn": true,
+		"fr-btn-primary": props.type === "primary",
+		"fr-btn-secondary": props.type === "secondary",
+		"fr-btn--small": props.small,
+		"fr-btn--disabled": props.disabled,
+		"fr-btn--loading": props.isLoading,
+		"fr-btn--active": props.isActive,
+		"fr-btn--radius": !props.isRounded,
+		"radius-max": props.isRounded
+	})
 
-		return (
-			<div className="inline-flex flex-ycenter relative" onClick={ e => this.props.action(e) }>
-				<input className={ buttonClasses } type="button" value={ this.props.value } />
-				{this.props.isLoading && <IconLoading color={ this.props.type === "secondary" && "var(--fr-500)" } />}
-				<style jsx global>{`
-					.fr-btn {
-						display: inline-block;
-						padding: 0.25rem 1rem;
+	return (
+		<div className="inline-flex flex-ycenter relative" onClick={ e => props.action(e) }>
+			<input className={ buttonClasses } type="button" value={ props.value } />
+			{props.isLoading && <IconLoading color={ props.type === "secondary" && "var(--fr-500)" } />}
+			<style jsx global>{`
+				.fr-btn {
+					display: inline-block;
+					padding: 0.25rem 1rem;
 
-						border: 0;
-						outline: none;
+					border: 0;
+					outline: none;
 
-						font-size: 0.875rem;
-						font-weight: 500;
-						line-height: 2rem;
-						cursor: pointer;
+					font-size: 0.875rem;
+					font-weight: 500;
+					line-height: 2rem;
+					cursor: pointer;
 
-						transition: background-color 100ms ease-in-out, box-shadow 100ms ease-in-out,
-							padding-left 180ms ease-in-out;
+					transition: background-color 100ms ease-in-out, box-shadow 100ms ease-in-out,
+						padding-left 180ms ease-in-out;
+				}
+
+				/* Primary */
+				.fr-btn-primary {
+					background-color: var(--fr-500);
+					color: var(--fr-ground);
+				}
+
+				.fr-btn-primary:not(.fr-btn--active):not(.fr-btn--loading):hover {
+					background-color: var(--fr-700);
+				}
+
+				.fr-btn.fr-btn-primary.fr-btn--active,
+				.fr-btn.fr-btn-primary:active,
+				.fr-btn.fr-btn-primary:active:focus {
+					background-color: var(--fr-900);
+					color: var(--fr-100);
+				}
+
+				.fr-btn-primary.fr-btn--loading {
+					color: var(--fr-300);
+				}
+
+				.fr-btn-primary.fr-btn--disabled {
+					background-color: var(--fr-100);
+					color: var(--fr-300);
+				}
+
+				/* Secondary */
+				.fr-btn-secondary {
+					background-color: var(--fr-ground);
+					box-shadow: inset 0 0 0 1px var(--fr-300);
+					color: var(--fr-500);
+				}
+
+				.fr-btn-secondary:not(.fr-btn--active):not(.fr-btn--loading):hover {
+					box-shadow: inset 0 0 0 1px var(--fr-500);
+					color: var(--fr-700);
+				}
+
+				.fr-btn.fr-btn-secondary.fr-btn--active,
+				.fr-btn.fr-btn-secondary:active {
+					background-color: var(--fr-100);
+					box-shadow: inset 0 0 0 1px var(--fr-500);
+					color: var(--fr-900);
+				}
+
+				.fr-btn-secondary.fr-btn--disabled {
+					box-shadow: inset 0 0 0 1px var(--fr-100);
+					color: var(--fr-300);
+				}
+
+				/* Default radius */
+				.fr-btn--radius {
+					border-radius: 0.25rem;
+				}
+
+				/* Small */
+				.fr-btn.fr-btn--small {
+					padding: 0.125rem 0.5rem;
+
+					font-size: 0.75rem;
+					line-height: 1.5rem;
+				}
+
+				/* Disabled */
+				.fr-btn--disabled {
+					pointer-events: none;
+				}
+
+				/* Loading */
+				.fr-btn--loading {
+					padding-left: 2.5rem;
+
+					pointer-events: none;
+				}
+
+				.fr-btn--small.fr-btn--loading {
+					padding-left: 2rem;
+				}
+
+				.fr-btn--small + .fr-btn__loading {
+					left: 0.125rem;
+				}
+
+				.fr-btn__loading {
+					position: absolute;
+					left: 0.5rem;
+					top: 0.5rem;
+					z-index: 1;
+
+					height: 1.5rem;
+					width: 1.5rem;
+
+					animation: fr-button-show 180ms ease-in-out;
+				}
+
+				.fr-btn__loading circle {
+					animation: fr-button-dash 480ms linear forwards infinite;
+				}
+
+				@keyframes fr-button-show {
+					0% {
+						opacity: 0;
 					}
-
-					/* Primary */
-					.fr-btn-primary {
-						background-color: var(--fr-500);
-						color: var(--fr-ground);
+					100% {
+						opacity: 1;
 					}
+				}
 
-					.fr-btn-primary:not(.fr-btn--active):not(.fr-btn--loading):hover {
-						background-color: var(--fr-700);
+				@keyframes fr-button-dash {
+					from {
+						stroke-dashoffset: 0;
 					}
-
-					.fr-btn.fr-btn-primary.fr-btn--active,
-					.fr-btn.fr-btn-primary:active,
-					.fr-btn.fr-btn-primary:active:focus {
-						background-color: var(--fr-900);
-						color: var(--fr-100);
+					to {
+						stroke-dashoffset: 64;
 					}
+				}
 
-					.fr-btn-primary.fr-btn--loading {
-						color: var(--fr-300);
-					}
+				/* Focus */
+				.fr-btn:focus {
+					z-index: 1;
+				}
 
-					.fr-btn-primary.fr-btn--disabled {
-						background-color: var(--fr-100);
-						color: var(--fr-300);
-					}
+				.fr-btn.fr-btn--small:focus {
+					box-shadow: 0 0 0 0.125rem var(--fr-focus);
+				}
 
-					/* Secondary */
-					.fr-btn-secondary {
-						background-color: var(--fr-ground);
-						box-shadow: inset 0 0 0 1px var(--fr-300);
-						color: var(--fr-500);
-					}
+				.fr-btn:focus:not(.fr-btn--disabled):not(.fr-btn--loading):not(.fr-btn--small) {
+					box-shadow: 0 0 0 0.1875rem var(--fr-focus);
+				}
 
-					.fr-btn-secondary:not(.fr-btn--active):not(.fr-btn--loading):hover {
-						box-shadow: inset 0 0 0 1px var(--fr-500);
-						color: var(--fr-700);
-					}
-
-					.fr-btn.fr-btn-secondary.fr-btn--active,
-					.fr-btn.fr-btn-secondary:active {
-						background-color: var(--fr-100);
-						box-shadow: inset 0 0 0 1px var(--fr-500);
-						color: var(--fr-900);
-					}
-
-					.fr-btn-secondary.fr-btn--disabled {
-						box-shadow: inset 0 0 0 1px var(--fr-100);
-						color: var(--fr-300);
-					}
-
-					/* Default radius */
-					.fr-btn--radius {
-						border-radius: 0.25rem;
-					}
-
-					/* Small */
-					.fr-btn.fr-btn--small {
-						padding: 0.125rem 0.5rem;
-
-						font-size: 0.75rem;
-						line-height: 1.5rem;
-					}
-
-					/* Disabled */
-					.fr-btn--disabled {
-						pointer-events: none;
-					}
-
-					/* Loading */
-					.fr-btn--loading {
-						padding-left: 2.5rem;
-
-						pointer-events: none;
-					}
-
-					.fr-btn--small.fr-btn--loading {
-						padding-left: 2rem;
-					}
-
-					.fr-btn--small + .fr-btn__loading {
-						left: 0.125rem;
-					}
-
-					.fr-btn__loading {
-						position: absolute;
-						left: 0.5rem;
-						top: 0.5rem;
-						z-index: 1;
-
-						height: 1.5rem;
-						width: 1.5rem;
-
-						animation: fr-button-show 180ms ease-in-out;
-					}
-
-					.fr-btn__loading circle {
-						animation: fr-button-dash 480ms linear forwards infinite;
-					}
-
-					@keyframes fr-button-show {
-						0% {
-							opacity: 0;
-						}
-						100% {
-							opacity: 1;
-						}
-					}
-
-					@keyframes fr-button-dash {
-						from {
-							stroke-dashoffset: 0;
-						}
-						to {
-							stroke-dashoffset: 64;
-						}
-					}
-
-					/* Focus */
-					.fr-btn:focus {
-						z-index: 1;
-					}
-
-					.fr-btn.fr-btn--small:focus {
-						box-shadow: 0 0 0 0.125rem var(--fr-focus);
-					}
-
-					.fr-btn:focus:not(.fr-btn--disabled):not(.fr-btn--loading):not(.fr-btn--small) {
-						box-shadow: 0 0 0 0.1875rem var(--fr-focus);
-					}
-
-					.fr-btn.fr-btn-secondary:focus:not(.fr-btn--disabled):not(.fr-btn--loading) {
-						box-shadow: inset 0 0 0 1px var(--fr-500), 0 0 0 0.1875rem var(--fr-focus);
-					}
-				`}</style>
-			</div>
-		)
-	}
+				.fr-btn.fr-btn-secondary:focus:not(.fr-btn--disabled):not(.fr-btn--loading) {
+					box-shadow: inset 0 0 0 1px var(--fr-500), 0 0 0 0.1875rem var(--fr-focus);
+				}
+			`}</style>
+		</div>
+	)
 }
 
 Button.propTypes = propTypes
