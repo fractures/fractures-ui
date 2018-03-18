50e51850074515fa40046f4a46622aeef9e3b75e
Krisztian Puska <pyx@pyx.space>
2018-03-18 13:12:38 +0100

    Fix styled-jsx throwing ss with multiple style tags

--- src/components/Fractures.js
+++ src/components/Fractures.js
@@ -1,33 +1,31 @@
 import { themes } from "../config/themes"
 import Button from "./Button"
 import fractures from "fractures"
-import React, { Fragment } from "react"
+import React from "react"
 
 const Fractures = props => {
 	const isThemed = props.theme
 	const theme = (isThemed && themes.find(theme => props.theme === theme.name)) || null
 
-	const themeRoot = theme && (
-		<style jsx global>{`
-			:root {
-				--fr-ground: ${ theme.colors.ground };
-				--fr-focus: ${ theme.colors.focus };
-				--fr-100: ${ theme.colors.shade100 };
-				--fr-300: ${ theme.colors.shade300 };
-				--fr-500: ${ theme.colors.shade500 };
-				--fr-700: ${ theme.colors.shade700 };
-				--fr-900: ${ theme.colors.shade900 };
-			}
-		`}</style>
-	)
+	const themeRoot =
+		theme &&
+		`
+		:root {
+			--fr-ground: ${ theme.colors.ground };
+			--fr-focus: ${ theme.colors.focus };
+			--fr-100: ${ theme.colors.shade100 };
+			--fr-300: ${ theme.colors.shade300 };
+			--fr-500: ${ theme.colors.shade500 };
+			--fr-700: ${ theme.colors.shade700 };
+			--fr-900: ${ theme.colors.shade900 };
+		}
+	`
 
 	return (
-		<Fragment>
-			<style jsx global>
-				{fractures}
-			</style>
+		<style jsx global>
+			{fractures}
 			{isThemed && theme ? themeRoot : null}
-		</Fragment>
+		</style>
 	)
 }
 
