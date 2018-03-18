edf095261c4879ad7a375d83f68e70921592fc51
Krisztian Puska <pyx@pyx.space>
2018-03-18 13:18:11 +0100

    Revert "Fix styled-jsx throwing ss with multiple style tags"
    This reverts commit 50e51850074515fa40046f4a46622aeef9e3b75e.

--- src/components/Fractures.js
+++ src/components/Fractures.js
@@ -1,31 +1,33 @@
 import { themes } from "../config/themes"
 import Button from "./Button"
 import fractures from "fractures"
-import React from "react"
+import React, { Fragment } from "react"
 
 const Fractures = props => {
 	const isThemed = props.theme
 	const theme = (isThemed && themes.find(theme => props.theme === theme.name)) || null
 
-	const themeRoot =
-		theme &&
-		`
-		:root {
-			--fr-ground: ${ theme.colors.ground };
-			--fr-focus: ${ theme.colors.focus };
-			--fr-100: ${ theme.colors.shade100 };
-			--fr-300: ${ theme.colors.shade300 };
-			--fr-500: ${ theme.colors.shade500 };
-			--fr-700: ${ theme.colors.shade700 };
-			--fr-900: ${ theme.colors.shade900 };
-		}
-	`
+	const themeRoot = theme && (
+		<style jsx global>{`
+			:root {
+				--fr-ground: ${ theme.colors.ground };
+				--fr-focus: ${ theme.colors.focus };
+				--fr-100: ${ theme.colors.shade100 };
+				--fr-300: ${ theme.colors.shade300 };
+				--fr-500: ${ theme.colors.shade500 };
+				--fr-700: ${ theme.colors.shade700 };
+				--fr-900: ${ theme.colors.shade900 };
+			}
+		`}</style>
+	)
 
 	return (
-		<style jsx global>
-			{fractures}
+		<Fragment>
+			<style jsx global>
+				{fractures}
+			</style>
 			{isThemed && theme ? themeRoot : null}
-		</style>
+		</Fragment>
 	)
 }
 
