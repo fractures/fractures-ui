6d6475c59b4a84e4158f6c056b4bbcc59af7216f
Krisztian Puska <pyx@pyx.space>
2018-04-02 18:45:13 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,7 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{
-				test: /\.(js)$/,
-				use: "babel-loader"
-			},
+			{ test: /\.(js)$/, use: "babel-loader" },
 			{ test: /\.css$/, loader: "raw-loader" }
 		]
 	},
