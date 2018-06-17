a992206b4bc9e07030cc040ae276185c420c17d7
Krisztian Puska <pyx@pyx.space>
2018-06-17 16:37:23 +0200

    Update babel

--- examples/Label.js
+++
@@ -1,9 +0,0 @@
-import Fractures from "./Fractures"
-import Label from "../src/components/Label/Label"
-import React from "react"
-
-export default () => (
-	<Fractures>
-		<Label>Label text</Label>
-	</Fractures>
-)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { Button, NakedButton } from "./components/Button/Button"
 import { Checkbox, NakedCheckbox } from "./components/Checkbox/Checkbox"
 import { Input, NakedInput } from "./components/Input/Input"
-import { Label, NakedLabel } from "./components/shared/Label/Label"
 import { Progress, NakedProgress } from "./components/Progress/Progress"
 import { Radio, NakedRadio } from "./components/Radio/Radio"
 import { Range, NakedRange } from "./components/Range/Range"
 import ButtonGroup from "./components/ButtonGroup/ButtonGroup"
-import Color from "./components/shared/Color/Color"
+import Color from "./components/Color/Color"
 import Container from "./components/Container/Container"
 import Fractures from "./components/Fractures/Fractures"
+import Label from "./components/Label/Label"
 
 export {
 	Button,
@@ -22,7 +22,6 @@ export {
 	NakedButton,
 	NakedCheckbox,
 	NakedInput,
-	NakedLabel,
 	NakedProgress,
 	NakedRadio,
 	NakedRange,
