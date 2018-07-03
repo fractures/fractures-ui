b19d7b7338f34708ac8d73f363fc733c2cc33adb
Krisztian Puska <pyx@pyx.space>
2018-07-03 15:11:28 +0200

    Fix tooltip test

--- src/components/Tooltip/Tooltip.test.js
+++ src/components/Tooltip/Tooltip.test.js
@@ -1,7 +1,7 @@
 import Tooltip from "./Tooltip.js"
 
 describe("Tooltip", () => {
-	const tree = renderer.create(<Tooltip>Content</Tooltip>).toJSON()
+	const tree = renderer.create(<Tooltip text="Tooltip">Content</Tooltip>).toJSON()
 
 	it("Renders correctly", () => expect(tree).toMatchSnapshot())
 })
